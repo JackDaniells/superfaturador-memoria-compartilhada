210c828fc0b5b49c74246966dfcd82c8d4af475c
dell <danielk.kock@live.com>
2017-06-27 16:33:40 -0300

    -

--- t3/signer-client/src/main/java/br/ufsc/ine5611/App.java
+++ t3/signer-client/src/main/java/br/ufsc/ine5611/App.java
@@ -2,6 +2,7 @@ package br.ufsc.ine5611;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
@@ -12,41 +13,35 @@ import java.nio.file.StandardOpenOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class App {
 
     private static MappedByteBuffer mappedByteBuffer;
+    private static ArrayList<String> command;
 
-    public static void main(String[] args) throws Exception {
-        List<String> command = new ArrayList<>();
-        command.add(args[0]);
-        File file = new File(args[1]);
-
-        ProcessBuilder processBuilder = new ProcessBuilder(command);
-        processBuilder.redirectInput(ProcessBuilder.Redirect.PIPE);
-        processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
-
-        Path pathToTemp = createTempFile(file);
-
-        final Process process = processBuilder.start();
-        SignerClient signerClient = new SignerClient(process.getOutputStream(), process.getInputStream());
-
-        signerClient.sign(pathToTemp.toFile());
-        process.waitFor();
-        signerClient.end();
-
-        readTempFileHash(file);
-    }
-
-    private static Path createTempFile(File file) {
-        Path path = null;
+    public static void main(String[] args){
+        
         try {
-            path = Files.createTempFile("temp", null);
-
+            
+            command = new ArrayList<>();
+            command.add(args[0]);
+            File file = new File(args[1]);
+            
+            
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.redirectInput(ProcessBuilder.Redirect.PIPE);
+            processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
+            
+            
+            Path pathToTemp = Files.createTempFile("temp", null);
+            
             FileChannel fileChannel = (FileChannel) Files.newByteChannel(
-                path,
+                pathToTemp,
                 EnumSet.of(StandardOpenOption.READ, StandardOpenOption.WRITE)
             );
+            
             mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, 4 + file.length() + 32);
             fileChannel.close();
 
@@ -63,37 +58,41 @@ public class App {
                 for (int i = 0; i < arrayBytes.length; i++) mappedByteBuffer.put(4 + i, arrayBytes[i]);
 
                 for (int i = 0; i < 32; i++) mappedByteBuffer.put(4 + (int) file.length() + i, (byte) 0);
+
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            
+            final Process process = processBuilder.start();
+            
+            SignerClient signerClient = new SignerClient(process.getOutputStream(), process.getInputStream());
+            
+            signerClient.sign(pathToTemp.toFile());
+            
+            process.waitFor();
+            
+            signerClient.end();
+            
+            byte[] signature = new byte[32];
+
+            for (int i = 0; i < signature.length; i++) {
+                signature[i] = mappedByteBuffer.get(4 + (int)file.length() + i);
+            }
+
+            System.out.println(Base64.getEncoder().encodeToString(signature));
+            
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            
+            FileInputStream in = new FileInputStream(file);
+            
+            while (in.available() > 0) md.update((byte) in.read());
+
+            byte[] originalFileSignature = md.digest();
+
+            System.out.println(Arrays.equals(originalFileSignature, signature)); 
+           
+        //exception
+        } catch (SignerException | IOException | InterruptedException | NoSuchAlgorithmException ex) {
+           System.err.println(ex.getMessage());
         }
-        return path;
     }
 
-    private static void readTempFileHash(File file) throws IOException {
-        byte[] signature = new byte[32];
-
-        for (int i = 0; i < signature.length; i++) {
-            signature[i] = mappedByteBuffer.get(4 + (int)file.length() + i);
-        }
-
-        System.out.println(Base64.getEncoder().encodeToString(signature));
-
-        byte[] originalFileSignature = getExpectedSignature(file);
-        System.out.println(Arrays.equals(originalFileSignature, signature));
-    }
-
-    private static byte[] getExpectedSignature(File file) throws IOException {
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Unexpected exception", e);
-        }
-        try (FileInputStream in = new FileInputStream(file)) {
-            while (in.available() > 0)
-                md.update((byte) in.read());
-        }
-        return md.digest();
-    }
 }
